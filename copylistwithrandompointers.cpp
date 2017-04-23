ab09836b52fa78e8bd7559d0565ec6c4fdac96d0
rahul satish <rahulsatish13@gmail.com>
2017-04-24 00:29:22 +0530

    copylistwithrandompointers

---
+++ copylistwithrandompointers.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *     int label;
+ *     RandomListNode *next, *random;
+ *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    RandomListNode *copyRandomList(RandomListNode *head) {
+        if(head==NULL)
+        {
+            return NULL;
+        }
+      
+        for(RandomListNode* temp=head;temp!=NULL;temp=temp->next->next)
+        {
+            RandomListNode* t=new RandomListNode(temp->label);
+            t->next=temp->next;
+            temp->next=t;
+        }
+        for(RandomListNode* temp=head;temp!=NULL;temp=temp->next->next)
+        {
+            if(temp->random!=NULL)
+            temp->next->random=temp->random->next;
+        }
+          RandomListNode* newhead=head->next;
+        RandomListNode* header=newhead;
+        RandomListNode* l1,*l2;
+   for(l1=head;l1!=NULL;l1=l1->next)
+   {
+       l2=l1->next;
+       l1->next=l2->next;
+       if(l2->next!=NULL)
+       {
+           l2->next=l2->next->next;
+       }
+   }
+
+        return newhead;
+    }
+};
